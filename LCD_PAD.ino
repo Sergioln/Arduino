bf22e7453923bc07c6adc93adb9e0733e12b1059
Sergioln <sergio.ln@gmail.com>
2016-06-15 18:07:00 +0200

    Update LCD_PAD.ino

--- LCD_PAD.ino
+++ LCD_PAD.ino
@@ -5,14 +5,16 @@
 #include <SFE_BMP180.h>
 #include <Wire.h>
 
+/* Variables and constants */
+
 // Pin to read the DHT22
 #define DHT22_PIN 13
-
+// Variable to read the DHT status
+int chk;
 // Altitude in Madrid
 #define ALTITUDE 635
 
-
-// define some values used by the panel and buttons
+// define some values used by the LCD panel and buttons
 int lcd_key     = 0;
 int adc_key_in  = 0;
 #define btnRIGHT  0
@@ -35,26 +37,40 @@ int read_LCD_buttons()
  if (adc_key_in < 450)  return btnDOWN; 
  if (adc_key_in < 650)  return btnLEFT; 
  if (adc_key_in < 850)  return btnSELECT;  
-
- // For V1.0 comment the other threshold and use the one below:
-/*
- if (adc_key_in < 50)   return btnRIGHT;  
- if (adc_key_in < 195)  return btnUP; 
- if (adc_key_in < 380)  return btnDOWN; 
- if (adc_key_in < 555)  return btnLEFT; 
- if (adc_key_in < 790)  return btnSELECT;   
-*/
-
-
+ 
  return btnNONE;  // when all others fail, return this...
 }
-// Crea el objeto DHT (temperatura y humedad)
-dht DHT;
-// Crea el objeto pressure (Sensor BMP180)
-SFE_BMP180 pressure; 
+
+/* Create the objects */
+
+dht DHT; // Crea el objeto DHT (temperatura y humedad)
+SFE_BMP180 pressure; // Crea el objeto pressure (Sensor BMP180)
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);  // Crea el objeto LCD //
 
 void setup(){
 }
 
 void loop(){
+ Serial.print("DHT22, \t");
+ chk = DHT.read22(DHT22_PIN);
+ switch (chk)
+ {
+  case DHTLIB_OK:  
+   Serial.print("OK,\t"); 
+   break;
+  case DHTLIB_ERROR_CHECKSUM: 
+   Serial.print("Checksum error,\t"); 
+   break;
+  case DHTLIB_ERROR_TIMEOUT: 
+   Serial.print("Time out error,\t"); 
+   break;
+  default: 
+   Serial.print("Unknown error,\t"); 
+   break;
+}
+ 
+ +  // DISPLAY DATA on the LCD
+ +  Serial.print(DHT.humidity, 1);
+ +  Serial.print(",\t");
+ +  Serial.println(DHT.temperature, 1);
 }
